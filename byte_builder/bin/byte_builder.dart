710fe7f20c96afe9873d2ed17aecf8794618957f
CristianCeccon <cristian_ceccon45@hotmail.com>
2022-09-23 20:10:01 -0300

    Criação do projeto

---
+++ byte_builder/bin/byte_builder.dart
@@ -0,0 +1,5 @@
+import 'package:byte_builder/byte_builder.dart' as byte_builder;
+
+void main(List<String> arguments) {
+  print('Hello world: ${byte_builder.calculate()}!');
+}
---
+++ byte_builder/test/byte_builder_test.dart
@@ -0,0 +1,8 @@
+import 'package:byte_builder/byte_builder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
